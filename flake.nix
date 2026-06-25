cb396a1a24468b1e17538ea8fa57406e390a3b3c
NenyUX <nenyux.dev@gmail.com>
2026-06-25 19:33:39 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,63 +1,18 @@
-{ config, pkgs, inputs, ... }:
-
 {
-  imports = [ ./hardware-configuration.nix ];
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  programs.hyprland = {
-    enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    hyprland.url = "github:hyprwm/Hyprland";
+    caelestia-shell = {
+      url = "github:caelestia-dots/shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [ ./configuration.nix ];
+    };
   };
-
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
-
-  time.timeZone = "America/Caracas";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "es_VE.UTF-8";
-    LC_IDENTIFICATION = "es_VE.UTF-8";
-    LC_MEASUREMENT = "es_VE.UTF-8";
-    LC_MONETARY = "es_VE.UTF-8";
-    LC_NAME = "es_VE.UTF-8";
-    LC_NUMERIC = "es_VE.UTF-8";
-    LC_PAPER = "es_VE.UTF-8";
-    LC_TELEPHONE = "es_VE.UTF-8";
-    LC_TIME = "es_VE.UTF-8";
-  };
-
-  services.xserver.xkb = {
-    layout = "latam";
-    variant = "";
-  };
-
-  console.keyMap = "la-latin1";
-
-  users.users."neny" = {
-    isNormalUser = true;
-    description = "neny";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
-  };
-
-  nixpkgs.config.allowUnfree = true;
-
-  environment.systemPackages = with pkgs; [
-    brave
-    kitty
-    inputs.caelestia-shell.packages.${pkgs.system}.with-cli
-  ];
-
-  system.stateVersion = "26.05";
 }
